934f1c99632bfe22983b732a2c7b75db04ad644d
pgunnink <pietergunnink@gmail.com>
2022-07-20 11:48:48 +0200

    Bugfix

--- src/plotting.jl
+++ src/plotting.jl
@@ -4,7 +4,7 @@ function positionsPlottingXY(unit_cells, l::LatticeDescription)
     # gets absolute positions for plotting, with the ordering the same as returned by mapping
     positions, mapping_index = positionsLattice(unit_cells, l)
 
-    res = [(0.0, 0.0) for _ in 1:length(mappingAtoms(unit_cells))]
+    res = [[0.0, 0.0] for _ in 1:length(mappingAtoms(unit_cells))]
     for x in keys(l.basisAtoms)
         for i = 1:length(positions[x])
             res[mapping_index[x][i], :] = positions[x][i]
